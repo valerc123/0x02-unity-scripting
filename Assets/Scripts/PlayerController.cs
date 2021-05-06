d2331c2f19952ebca2f450458b3798d06f553517
Valentina Rua C <40582270+valerc123@users.noreply.github.com>
2021-05-06 00:40:32 -0500

    Insert coin

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    //public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void FixedUpdate(){
+       // rb.velocity = new Vector3(speed, 0, speed);
+
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce(Vector3.left);
+        }
+        else if(Input.GetKey(KeyCode.W)){
+            rb.AddForce(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            rb.AddForce(Vector3.back);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            rb.AddForce(Vector3.right);
+        }
+    }
+
+}
